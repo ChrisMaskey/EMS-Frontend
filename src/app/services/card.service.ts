2ccdf2b702d4b2e7239132d28f2d8a0bd3b223ce
Shreeja Pokharel <shreeja.pokharel1030@gmail.com>
2023-10-02 16:58:42 +0545

    changes in employee dialog ui

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,15 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Employee } from '../Model/employee.model';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CardService {
-  employee: Employee[] = [];
+  // employee: Employee[] = [];
 
   private apiUrl = 'https://vertex90-001-site1.atempurl.com';
+
+  private employeeSubject = new BehaviorSubject<Employee[] | null>(null);
+  public employee$ = this.employeeSubject.asObservable();
+  
 
   constructor(private http: HttpClient) {}
 
@@ -17,5 +21,36 @@ export class CardService {
     return this.http.get<Employee[]>(
       this.apiUrl + '/api/User/get-all-employees'
     );
+ 
   }
-}
+
+  // getEmployee():Promise<void> {
+  //   return new Promise((resolve,reject) => {
+  //     return this.http.get<Employee[]>( this.apiUrl + '/api/User/get-all-employees').subscribe((data:Employee[]) => {
+  //       this.employeeSubject.next(data)
+  //     })
+  //   })
+  }
+
+
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Employee } from '../Model/employee.model';
+// import { Observable } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class CardService {
+//   employee: Employee[] = [];
+
+//   private apiUrl = 'https://vertex90-001-site1.atempurl.com';
+
+//   constructor(private http: HttpClient) {}
+
+//   getEmployee(): Observable<Employee[]> {
+//     return this.http.get<Employee[]>(
+//       this.apiUrl + '/api/User/get-all-employees'
+//     );
+//   }
+// }
