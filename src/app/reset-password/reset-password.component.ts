0accb6d77f5d925df63c217d9d1e59fb68a4a62c
Shreejapokharel <shreeja@vertexspecial.com>
2023-10-05 21:52:14 +0545

    reset test

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-reset-password',
@@ -14,12 +14,11 @@ export class ResetPasswordComponent {
   confirmPassword: string = '';
   message: string = '';
   messageType: string = '';
-  apiUrl = 'http://vertex90-001-site1.atempurl.com/api/Email/reset-password'; // Note: Changed to HTTP instead of HTTPS
+  apiUrl = 'https://vertex90-001-site1.atempurl.com/api/Email/reset-password';
 
   constructor(
     private route: ActivatedRoute,
     private http: HttpClient,
-    private router: Router
   ) {
     this.route.queryParams.subscribe(params => {
       this.activationToken = params['activationToken'];
@@ -30,45 +29,45 @@ export class ResetPasswordComponent {
   }
 
   onSubmit() {
-    if (this.newPassword === this.confirmPassword) {
-      const data = {
-        email: this.email,
-        password: this.newPassword,
-        confirmPassword: this.confirmPassword,
-        token: this.activationToken
-      };
-
-      const headers = new HttpHeaders()
-        .set('Content-Type', 'application/json');
-
-      this.http.post(this.apiUrl, data, { headers, responseType: 'json' }).subscribe(
-        (response: any) => { // Change response type to 'any'
-          if (response.code === '200') {
+    const data = {
+      email: this.email,
+      password: this.newPassword,
+      confirmPassword: this.confirmPassword,
+      token: this.activationToken
+    };
+  
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json');
+  
+    this.http.post(this.apiUrl, data, { headers, responseType: 'text' }).subscribe(
+      (response: any) => { // Change the response type to 'any'
+        if (response !== null) {
+          const responseData = JSON.parse(response);
+          if (responseData.code === '200') {
             console.log('Password reset successful.');
             this.messageType = 'success';
-            this.message = response.description;
-            this.handleApiSuccess();
+            this.message = responseData.description;
           } else {
-            console.error('Unexpected response:', response);
+            console.error('Unexpected response:', responseData);
             this.messageType = 'error';
             this.handleApiError('Unexpected response from the server.');
           }
-        },
-        (error: HttpErrorResponse) => {
-          console.error('Error resetting password', error);
+        } else {
+          console.error('Response is null or empty');
           this.messageType = 'error';
-          this.handleApiError('An error occurred while resetting the password. Please try again later.');
+          this.handleApiError('Response from the server is null or empty.');
         }
-      );
-    } else {
-      this.messageType = 'error';
-      this.message = 'Passwords do not match.';
-    }
+      },
+      (error) => {
+        console.error('Error resetting password', error);
+        this.messageType = 'error';
+        this.handleApiError(
+          'An error occurred while resetting the password. Please try again later.'
+        );
+      }
+    );
   }
-
-  private handleApiSuccess() {
-    this.router.navigate(['/login']);
-  }
+  
 
   private handleApiError(errorMessage: string) {
     this.messageType = 'error';
