40fef131c98fe1b80832415acdab56591a398392
Shreejapokharel <shreeja@vertexspecial.com>
2023-10-20 19:25:14 +0545

    added authguards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,27 +7,22 @@ import { AddComponent } from './employee-list/add/add.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { TestComponent } from './test/test.component';
 import { HierarchyComponent } from './hierarchy/hierarchy.component';
-import { OrganizationalChartComponent } from './organizational-chart/organizational-chart.component';
 import { SuperadminComponent } from './employee-list/superadmin/superadmin.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
+import { AuthGuard } from './guard/authguard.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'list', component: EmployeeListComponent },
   { path: 'login', component: LoginComponent },
   { path: 'add', component: AddComponent },
-  { path: 'home', component: CardComponent },
-  { path: 'card', component: CardComponent },
+  { path: 'card', component: CardComponent, canActivate: [AuthGuard] },
   { path: 'forgotpassword', component: ForgotPasswordComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
-  { path: 'test', component: TestComponent },
-  {path: 'hierarchy', component: HierarchyComponent },
-  { path: 'organizational-chart', component: OrganizationalChartComponent },
-  { path: 'test', component: TestComponent },
+  { path: 'hierarchy', component: HierarchyComponent, canActivate: [AuthGuard] },
   { path: 'superadmin', component: SuperadminComponent },
-  { path: 'change-password', component: ChangePasswordComponent}
+  { path: 'change-password', component: ChangePasswordComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
